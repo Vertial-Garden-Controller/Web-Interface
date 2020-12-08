132f5cbe73c2bd74d721a3c4069f14b3147cf20c
Joshua Barringer <barrinjo@oregonstate.edu>
2020-12-07 17:50:51 -0800

    updated file system layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './utils/App.css';
 import {
   BrowserRouter as Router,
   Switch,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import './utils/index.css';
+import reportWebVitals from './utils/reportWebVitals';
 import App from './App'
 
 ReactDOM.render(
